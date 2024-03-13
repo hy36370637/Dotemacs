9c0a761f4b7d576c7a13320b047da7d430a1ffab
호영 <149951378+hy36370637@users.noreply.github.com>
2024-03-13 13:44:27 +0900

    add agenda custom view
    날짜 포맷을  변경

--- init.el
+++ init.el
@@ -137,7 +137,7 @@
 (defvar-keymap my-prefix-map
   :doc "my prefix map."
   "c" my-consult-map
-  "p" 'my-popmark
+  "t" 'my-popmark
   "e" 'eshell
   "m" 'modus-themes-toggle
   "f" 'toggle-frame-fullscreen
@@ -231,11 +231,18 @@
 ;; ======================================
 ;;; org
 ;; --------------------------------------
+;; Key bindings
 (use-package org
   :bind
   (("M-n" . outline-next-visible-heading)
-   ("M-p" . outline-previous-visible-heading))
+   ("M-p" . outline-previous-visible-heading)
+   ("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c c" . org-capture))
+  
+  ;; Customizations
   :custom
+  ;; General org-mode settings
   (org-startup-indented nil)
   (org-hide-leading-stars nil)
   (org-startup-with-inline-images nil)
@@ -244,27 +251,74 @@
   (org-log-into-drawer t)
   (org-log-done 'time)
   (org-image-actual-width '(100))
-  :config
-  (setq org-directory (expand-file-name (if my-laptop-p "~/eDoc/org/" "~/Dropbox/eDoc/org/")))
-  (setq org-agenda-files '("Tasks.org" "Daily.org"))
-  (setq org-todo-keywords '((sequence "TODO" "HOLD" "DONE")))
+
+  ;; Org directory and agenda files
+  (org-directory (expand-file-name (if my-laptop-p "~/eDoc/org/" "~/Dropbox/eDoc/org/")))
+  (org-agenda-files '("Tasks.org" "Daily.org"))
+  
+  ;; Todo keywords
+  (org-todo-keywords '((sequence "TODO" "HOLD" "DONE")))
+  
   ;; Capture templates
-  (setq org-capture-templates
-        '(("d" "Daily" entry (file+datetree "Daily.org") "* %?")
-          ("t" "Tasks" entry (file+olp "Tasks.org" "Schedule") "* TODO %?")
-          ("f" "dFarmNote" entry (file+datetree "dFarmNote.org") "* %?")))
+  (org-capture-templates
+    '(("d" "Daily" entry (file+datetree "Daily.org") "* %?")
+      ("t" "Tasks" entry (file+olp "Tasks.org" "Schedule") "* TODO %?")
+      ("f" "dFarmNote" entry (file+datetree "dFarmNote.org") "* %?")))
+  
   ;; Export settings
-  (setq org-latex-title-command "\\maketitle \\newpage"
-        org-latex-toc-command "\\tableofcontents \\newpage"
-        org-latex-compiler "xelatex"
-        org-latex-to-pdf-process
-        '("xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f"
-          "xelatex -interaction nonstopmode -output-directory %o %f")))
-;; Global key bindings
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
+  (org-latex-title-command "\\maketitle \\newpage")
+  (org-latex-toc-command "\\tableofcontents \\newpage")
+  (org-latex-compiler "xelatex")
+  (org-latex-to-pdf-process
+    '("xelatex -interaction nonstopmode -output-directory %o %f"
+      "xelatex -interaction nonstopmode -output-directory %o %f"
+      "xelatex -interaction nonstopmode -output-directory %o %f"))
+  
+  ;; Agenda view customizations
+  (org-agenda-custom-commands
+    '(("d" "Custom agenda view"
+       ((agenda "" ((org-agenda-span 'week)
+                    (org-agenda-start-on-weekday 0)
+                    (org-agenda-format-date "%Y-%m-%d"))))))))
+
+;; (use-package org
+;;   :bind
+;;   (("M-n" . outline-next-visible-heading)
+;;    ("M-p" . outline-previous-visible-heading)
+;;    ("C-c l" . org-store-link)
+;;    ("C-c a" . org-agenda)
+;;    ("C-c c" . org-capture))
+;;   :custom
+;;   (org-startup-indented nil)
+;;   (org-hide-leading-stars nil)
+;;   (org-startup-with-inline-images nil)
+;;   (org-adapt-indentation t)
+;;   (org-src-preserve-indentation t)
+;;   (org-log-into-drawer t)
+;;   (org-log-done 'time)
+;;   (org-image-actual-width '(100))
+;;   (org-directory (expand-file-name (if my-laptop-p "~/eDoc/org/" "~/Dropbox/eDoc/org/")))
+;;   (org-agenda-files '("Tasks.org" "Daily.org"))
+;;   (org-todo-keywords '((sequence "TODO" "HOLD" "DONE")))
+;;   ;; Capture templates
+;;   (org-capture-templates
+;;     '(("d" "Daily" entry (file+datetree "Daily.org") "* %?")
+;;       ("t" "Tasks" entry (file+olp "Tasks.org" "Schedule") "* TODO %?")
+;;       ("f" "dFarmNote" entry (file+datetree "dFarmNote.org") "* %?")))
+;;   ;; Export settings
+;;   (org-latex-title-command "\\maketitle \\newpage")
+;;   (org-latex-toc-command "\\tableofcontents \\newpage")
+;;   (org-latex-compiler "xelatex")
+;;   (org-latex-to-pdf-process
+;;     '("xelatex -interaction nonstopmode -output-directory %o %f"
+;;       "xelatex -interaction nonstopmode -output-directory %o %f"
+;;       "xelatex -interaction nonstopmode -output-directory %o %f"))
+;;   ;; Agenda view customizations
+;;   (org-agenda-custom-commands
+;;     '(("d" "Custom agenda view"
+;;        ((agenda "" ((org-agenda-span 'week)
+;;                     (org-agenda-start-on-weekday 0)
+;;                     (org-agenda-format-date "%Y-%m-%d"))))))))
 ;;
 ;; ======================================
 ;;; org-bullets
@@ -290,7 +344,7 @@
    (t (message "err,, please enter 8, 9, or 0.")))
   )
 
-(global-set-key (kbd "C-1") 'org-custom-action)
+(global-set-key (kbd "C-0") 'org-custom-action)
 ;;
 ;; ======================================
 ;;; which-key
@@ -540,7 +594,7 @@
         ("C-c f" . toggle-frame-fullscreen)
         ("<tab>" . dired-subtree-toggle)
         ("<backtab>" . dired-subtree-cycle))
-  :hook (dired-after-readin . sof/dired-sort))
+  :hook (dired-after-readin . sof/dired-sort)) ;dired-mode에서 파일 로드 후 sof함수 호출
 ;; ======================================
 ;;; dired-narrow
 ;; --------------------------------------
