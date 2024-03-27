e8cfa585c5ad638cfe86ef20b57e34c00f8f2212
호영 <149951378+hy36370637@users.noreply.github.com>
2024-03-27 13:02:14 +0900

    vlc 설치여부 확인

--- my-play-streaming.el
+++ my-play-streaming.el
@@ -20,19 +20,21 @@
       (stop-streaming)
     (play-start-streaming (read-url-from-file (if my-mactop-p "~/Dropbox/Mp3/mmslist.txt" "~/emacs/mmslist.txt")))))
 
-(defun play-start-streaming (url)
+(defun play-start-streaming (url)  ;vlc Check version
   "Start streaming audio from a given URL using VLC."
   (interactive "sURL: ")
-  (if (not stream-process)
-      (let* ((vlc-command (if (eq system-type 'darwin)
-                              "/Applications/VLC.app/Contents/MacOS/VLC" ; for macOS
-                            "vlc"))                                      ; for linux
-	     (chosen-title (get-chosen-title (if my-mactop-p "~/Dropbox/Mp3/mmslist.txt" "~/emacs/mmslist.txt"))))
-        (setq stream-process (start-process "vlc" nil vlc-command "--no-video" "-I" "rc" url)) ; background play
-        (set-process-query-on-exit-flag stream-process nil)
-        (message "Playing: %s" chosen-title)
-        (setq stream-playing t))
-    (message "Streaming is already playing.")))
+  (let* ((vlc-command (if (eq system-type 'darwin)
+                          "/Applications/VLC.app/Contents/MacOS/VLC" ; for macOS
+                        "vlc")))                                     ; for linux
+    (if (not (file-executable-p vlc-command))   ;vlc 존재 여부
+        (message "VLC is not installed. Please install VLC to stream audio.")
+      (if (not stream-process)
+          (let* ((chosen-title (get-chosen-title (if my-mactop-p "~/Dropbox/Mp3/mmslist.txt" "~/emacs/mmslist.txt"))))
+            (setq stream-process (start-process "vlc" nil vlc-command "--no-video" "-I" "rc" url)) ; background play
+            (set-process-query-on-exit-flag stream-process nil)
+            (message "Playing: %s" chosen-title)
+            (setq stream-playing t))
+        (message "Streaming is already playing.")))))
 
 (defun stop-streaming ()
   "Stop the currently running VLC process."
@@ -54,10 +56,10 @@
          (chosen-title (completing-read "Choose a title to play: " titles)))
     chosen-title))
 
-;; (defun edit-mmslist ()
-;;   "Edit the mmslist.txt file."
-;;   (interactive)
-;;   (find-file (if my-mactop-p "~/Dropbox/Mp3/mmslist.txt" "~/emacs/mmslist.txt")))
+(defun edit-mmslist ()
+  "Edit the mmslist.txt file."
+  (interactive)
+  (find-file (if my-mactop-p "~/Dropbox/Mp3/mmslist.txt" "~/emacs/mmslist.txt")))
 
 (defun read-url-from-file (file)
   "Read streaming URLs from a file and return a URL chosen by the user."
