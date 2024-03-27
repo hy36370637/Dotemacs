bbd2a065d72b30da25a5e06e460e9b4cbeea9f70
호영 <149951378+hy36370637@users.noreply.github.com>
2024-03-27 18:38:53 +0900

    Add files via upload

---
+++ my-reading-mode-custom.el
@@ -0,0 +1,36 @@
+;; ======================================
+;;; view-mode
+;; ======================================
+;; 읽기 모드, 편집 보호
+(use-package view
+  :ensure nil    ;built-in
+  :init
+  (setq view-read-only t)
+  :bind
+  (:map view-mode-map
+        ("n" . View-scroll-line-forward)
+        ("p" . View-scroll-line-backward)))
+;;
+;; ======================================
+;;; my-reading(view)-mode
+;; ======================================
+;; "Read-only mode, not editable."
+;; I drew inspiration from novel-mode and view-mode.
+;; Therefore, you should refer to the view-mode settings.
+(defun toggle-my-reading-mode ()
+  "Toggle fullscreen & view-mode."
+  (interactive)
+  (if (and (boundp 'my-reading-mode-enabled) my-reading-mode-enabled)
+      (progn
+        (toggle-frame-fullscreen)
+        (text-scale-decrease 0.5)
+        (setq my-reading-mode-enabled nil)
+        (view-mode -1))
+    (progn
+      (unless (eq (frame-parameter nil 'fullscreen) 'fullboth)
+        (toggle-frame-fullscreen))
+      (text-scale-increase 0.5)
+      (setq my-reading-mode-enabled t)
+      (view-mode))))
+
+(provide 'my-reading-mode-custom)
