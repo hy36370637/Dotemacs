009041747447d4091684a111fb203234a45c7c33
호영 <149951378+hy36370637@users.noreply.github.com>
2024-07-01 17:22:06 +0900

    select-special-character 더함
    특수문자 목록선택 입력 함수

--- my-completion.el
+++ my-completion.el
@@ -8,14 +8,19 @@
   :ensure nil  ;built in
   :config
   (setq electric-pair-pairs '((?\" . ?\")
+                              (?\` . ?\`)  ;; 이 라인은 백틱(`)를 쌍으로 추가
+                              (?\' . ?\') 
                               (?\{ . ?\})
                               (?\[ . ?\])
                               (?\( . ?\))))
   (electric-pair-mode t))
-(add-hook 'org-mode-hook (lambda ()   ; pair-mode '< '제외
-			   (setq-local electric-pair-inhibit-predicate
-				       `(lambda (c)
-					  (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+
+(add-hook 'org-mode-hook (lambda ()
+                           (setq-local electric-pair-inhibit-predicate
+                                       (lambda (c)
+                                         (if (char-equal c ?<)
+                                             t
+                                           (funcall (default-value 'electric-pair-inhibit-predicate) c))))))
 
 ;; =======================================
 ;;; corfu
@@ -51,21 +56,30 @@
 (use-package abbrev
   :ensure nil
   :config
-  (setq-default abbrev-mode t)
+  (setq-default abbrev-mode nil)
   (setq save-abbrevs 'silently)        ;; save abbrevs when files are saved
   (abbrev-table-put global-abbrev-table :regexp "\\(?:^\\|[\t\s]+\\)\\(?1:[:;_].*\\|.*\\)")
-  ;; 약어 정의
   (define-abbrev-table 'global-abbrev-table 
     '( ("ldot"        " ……")
        ("notosans"     "Noto Sans CJK KR")
-       ("notosansmono"  "Noto Sans Mono CJK KR" )
-       ("notoserif"    "Noto Serif CJK KR")
-       ("hcrbatang"  "HCRBatangLVT")
+       ("notosansmono"   "Noto Sans Mono CJK KR" )
+       ("notoserif"      "Noto Serif CJK KR")
+       ("hcrbatang"    "HCRBatangLVT")
        ("sectionroman"   "\renewcommand*{\thesection}{\Roman{section}")
        ("subsectionarabic"  "\renewcommand*{\thesubsection}{\arabic{subsection}")
+       ("b_q" "#+bebin_quote")
+       ("e_q" "#+end_quote")
        )))
-  
+
+(defun select-special-character ()
+  "Prompt the user to select a special character and insert it at point."
+  (interactive)
+  (let ((characters '("·" "→" "「」" "※")))
+    (insert (completing-read "Select special character: " characters))))
 
 
+
+
+;; end here
 (provide 'my-completion)
 
