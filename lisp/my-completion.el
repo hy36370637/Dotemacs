33d3c45fb00302afd0708c42f6a430101478d1e0
호영 <149951378+hy36370637@users.noreply.github.com>
2024-08-06 06:02:21 +0900

    example org-table for abbrev

--- lisp/my-completion.el
+++ lisp/my-completion.el
@@ -35,19 +35,22 @@
          ("S-TAB" . corfu-previous)
          ("S-<return>" . corfu-insert))
   :custom
-  (corfu-auto t)
-  (corfu-auto-delay 0.2)
-  (corfu-auto-prefix 0)
-  (corfu-cycle t)
-  (corfu-preselect 'prompt)
-  (corfu-echo-documentation 0.2)
+  (corfu-auto t)                   ;enable auto completion
+  ;; (corfu-auto-delay 0.2)
+  ;; (corfu-auto-prefix 0)
+  (corfu-cycle t)			
+  ;; (corfu-preselect 'prompt)
+  ;; (corfu-echo-documentation 0.2)
   (corfu-preview-current 'insert)
-  (corfu-separator ?\s)
-  (corfu-quit-no-match 'separator)
+  (corfu-separator ?\s)			;orderless field separator
+  (corfu-quit-no-match 'separator)	;Automatically quit if there is no match
   :init
   (global-corfu-mode)
-  (corfu-history-mode)
-  (corfu-popupinfo-mode))
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  :hook (eshell-mode . (lambda ()
+                         (setq-local corfu-auto t)
+                         (corfu-mode 1))))
 
 ;; =======================================
 ;;; abbrev
@@ -56,28 +59,37 @@
 (use-package abbrev
   :ensure nil
   :config
-  (setq-default abbrev-mode nil)
-  (setq save-abbrevs 'silently)        ;; save abbrevs when files are saved
+  (setq-default abbrev-mode t)
+  (setq save-abbrevs nil)
+  ;; (setq save-abbrevs 'silently)        ;; save abbrevs when files are saved
   (abbrev-table-put global-abbrev-table :regexp "\\(?:^\\|[\t\s]+\\)\\(?1:[:;_].*\\|.*\\)")
   (define-abbrev-table 'global-abbrev-table 
-    '(("notosans"     "Noto Sans CJK KR")
-       ("notosansmono"   "Noto Sans Mono CJK KR" )
-       ("notoserif"      "Noto Serif CJK KR")
-       ("hcrbatang"    "HCRBatangLVT")
-       ("sectionroman"   "\renewcommand*{\thesection}{\Roman{section}")
-       ("subsectionarabic"  "\renewcommand*{\thesubsection}{\arabic{subsection}")
-       ("b_q" "#+bebin_quote")
-       ("e_q" "#+end_quote")
-       )))
-
+    '(("m2"  "㎡")   ("km"  "㎞")("lDot" "……") 
+      ("cX"    "×")   ("cB"   "※")
+      ("lG"    "「")   ("rG"    "」")
+      ("llG"  "『")   ("rrG"   "』")
+      ("cZ"   "○")   ("cQ"   "□")
+      ))
+;; Org 모드 약어 테이블 설정
+  ;; (with-eval-after-load 'org
+  ;;   (define-abbrev-table 'org-mode-abbrev-table
+  ;;     '(("todo" "TODO")
+  ;; 	("sch" "SCHEDULED:")
+  ;; 	("dl" "DEADLINE:")
+  ;; 	("pr" "PROPERTIES:")
+  ;; 	("src" "#+BEGIN_SRC\n\n#+END_SRC")
+  ;; 	("quote" "#+BEGIN_QUOTE\n\n#+END_QUOTE")
+  ;;   )))
+)
+  
 ;; =======================================
 ;;; 특수문자 입력
 ;; ======================================-
 (defun select-special-character ()
   "Prompt the user to select a special character and insert it at point."
   (interactive)
-  (let ((ch '("·" "→" "「」" "※" "…" "―")))
-    (insert (completing-read "문자 선택: " ch))))
+  (let ((choi '("·"  "→"  "⇒"  "「」"  "『』"  "※"  "…"  "―")))
+    (insert (completing-read "선택: " choi))))
 
 ;; =======================================
 ;;; Hunspell 설정
@@ -85,21 +97,18 @@
 ;; 한글 맞춤법
 (use-package ispell
   :if my-mactop-p
+  :hook (text-mode . flyspell-mode)
   :config
   (setq ispell-program-name "hunspell")
   (setq ispell-local-dictionary "ko_KR")
   (setq ispell-local-dictionary-alist
         '(("ko_KR" "[가-힣]" "[^가-힣]" "[-']" nil ("-d" "ko_KR") nil utf-8))))
 
-;; 맞춤법 검사 함수 정의 - 수동 검사
-(defun my-hunspell-check ()
-  "Run Hunspell spell check on the current buffer."
-  (interactive)
-  (ispell-buffer))
-
-;; Flyspell 모드 활성화 방법
-;;(add-hook 'text-mode-hook 'flyspell-mode)
-;; (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;; ;; 맞춤법 검사 함수 정의 - 수동 검사
+;; (defun my-hunspell-check ()
+;;   "Run Hunspell spell check on the current buffer."
+;;   (interactive)
+;;   (ispell-buffer))
 
 
 
