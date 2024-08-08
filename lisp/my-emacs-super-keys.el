7e46e29ff0bd143dbdd31f04fa3c8e2a8e72b491
호영 <149951378+hy36370637@users.noreply.github.com>
2024-08-09 05:21:48 +0900

    추가 defvar-keymap my-prefix-map
    ini.el 에 있는 것을 정리하여 단순화

--- lisp/my-emacs-super-keys.el
+++ lisp/my-emacs-super-keys.el
@@ -1,42 +1,28 @@
 ;;; -*- lexical-binding: t; -*-
 ;;; 자주 사용 → Super key 할당 → minor mode
 ;;;  I was inspired by the https://protesilaos.com
-;;; unset-super-key
-;; (global-unset-key (kbd "s-t"))    ; set font
-;; (global-unset-key (kbd "s-c"))    ; Copy
-;; (global-unset-key (kbd "s-e"))    ; isearch-yank-kill
-;; (global-unset-key (kbd "s-f"))     ; I-search forward
-;; (global-unset-key (kbd "s-g"))    ;
-;; (global-unset-key (kbd "s-o"))    ;
-;; (global-unset-key (kbd "s-p"))    ; ns-print-buffer
-;; (global-unset-key (kbd "s-l"))     ; goto-line
-;; (global-unset-key (kbd "s-m"))     ; 
-;; (global-unset-key (kbd "s-n"))    ; make-frame
-;; (global-unset-key (kbd "s-x"))     ; kill-region
-;; (global-unset-key (kbd "s-z"))     ; undo(C-/, C-x u)
-;;; 본래 할당키 목록
-;; "s-w" delete-frame, "s-q" save-buffers-kill-emacs, "s-t" unbind, "s-y" ns-paste-secondary, "s-u" revert-buffer, "s-i" unbind, "s-o" other-window, "s-p" ns-print-buffer, "s-a" mark-whole-buffer, "s-s" save-buffer, "s-d" consult-dir, "s-f" toggle-frame-fullscreen, "s-g" consult-grep, "s-;" unbind, "s-'" next-window-any-frame, "s-z" unbind, "s-x" unbind,  "s-v" yank, "s-b" switch-to-buffer, "s-n" make-frame, "s-m" my-prefix-map, "s-," unbind, "s-." unbind, "s-/" undo-redo
-
 (defvar-keymap my-emacs-super-keys-map
   :doc "Common command alternatives using the Super key."
-  ;; "s-a" #'mark-whole-buffer
     "s-b" #'consult-buffer
-    ;; "s-c" #'org-capture
     "s-d" #'consult-dir
     "s-e" #'eshell
     "s-g" #'consult-grep
-    ;; "s-h" #'ns-do-hide-emacs   ;창 숨기기
-    ;; "s-j" #'exchange-point-and-mark(C-x C-x)
-    ;; "s-k" #'kill-current-buffer
-    "s-l" #'my-org-latex-custom  ;goto-line
     "s-n" #'find-file
     "s-o" #'org-insert-structure-template
-    ;; "s-p" #'my-popmark    ;#'ns-print-buffer
-    ;; "s-q" #'save-buffers-kill-emacs  ;quit
     "s-r" #'toggle-streaming
     "s-z" #'repeat
     "s-<return>" #'toggle-frame-fullscreen
-    "s-/" #'undo-redo)
+    "s-/" #'undo-redo
+    "C-s-k" #'keycast-mode-line-mode
+    "C-s-r" #'toggle-my-reading-mode
+    "C-s-s" #'my/region-search-web
+    "C-s-w" #'my/naver-weather-search
+    "s-t c"  #'select-special-character
+    "s-t m" #'modus-themes-toggle
+    "s-t r" #'toggle-my-reading-mode
+    "s-t s" #'my/search-selected-text
+    "s-t w" #'my/naver-weather-search
+    )
 
   (define-minor-mode my-emacs-super-keys-mode  ; minor-mode
     "Provide Super key alternatives to common command."
