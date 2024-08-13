465a267b5e2b3db6ed7f89b06f11273b012bc8e1
HO-YOUNG KIM <hy36370637@gmail.com>
2024-08-13 13:42:32 +0900

    Initial commit with corrected .gitignore 충동해결

---
+++ lisp/my-emacs-hyper-keys.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t; -*-
+;;; Hyper key  → minor mode
+;;;  I was inspired by the https://protesilaos.com
+(defvar-keymap my-emacs-hyper-keys-map
+  :doc "My command alternatives using the Hyper key."
+    ;;; window
+    "H-w ]" #'enlarge-window-horizontally
+    "H-w ["  #'shrink-window-horizontally
+    "H-w ="  #'balance-windows
+    "H-w _"  #'shrink-window
+    "H-w +"  #'enlarge-window
+    
+    ;;; kmacro
+    "H-m ["  #'kmacro-start-macro
+    "H-m ]"  #'kmacro-end-macro
+    "H-'"   #'kmacro-end-and-call-macro  ;F4
+    )
+
+  (define-minor-mode my-emacs-hyper-keys-mode           ; minor-mode
+    "Provide Hyper key alternatives to common command."
+    :global t
+    :init-value t
+    :keymap my-emacs-hyper-keys-map)
+
+
+;; end here
+(provide 'my-emacs-hyper-keys)
