eb3c3eb8cc1241ce60545b6a64519c2a21b580ae
HO-YOUNG KIM <hy36370637@gmail.com>
2024-08-25 16:43:30 +0900

    Simplify which-key display

--- lisp/my-play-streaming.el
+++ lisp/my-play-streaming.el
@@ -1,5 +1,5 @@
 ;;; my-play-streaming.el --- Streaming and MP3 player for Emacs -*- lexical-binding: t; -*-
-;; Version: 1.5
+;; Version: 1.6
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: multimedia
 
@@ -10,6 +10,7 @@
 ;; Features a minor mode that integrates all MP3 player and streaming radio functions.
 
 (require 'cl-lib)
+(require 'which-key) ;Simplify which-key display
 
 ;;; Streaming Radio
 (defgroup my-streaming nil
@@ -65,19 +66,23 @@
 ;;; Integrated Music Player Minor Mode
 (defvar my-music-player-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "H-m p") #'my-music-player-play-pause)
-    (define-key map (kbd "H-m s") #'my-music-player-stop)
-    (define-key map (kbd "H-m n") #'my-music-player-next)
-    (define-key map (kbd "H-m b") #'my-music-player-previous)
-    (define-key map (kbd "H-m r") #'my-music-player-repeat-toggle)
-    (define-key map (kbd "H-m f") #'my-music-player-shuffle-toggle)
-    (define-key map (kbd "H-m l") #'my-music-player-load-directory)
-    (define-key map (kbd "H-m i") #'my-music-player-show-current)
-    (define-key map (kbd "H-m v") #'my-music-player-view-playlist)
-    (define-key map (kbd "H-m t") #'my-music-player-toggle-streaming)
-    (define-key map (kbd "H-m q") #'my-music-player-quit)
+   (define-key map (kbd "C-c m p") #'my-music-player-play-pause)
+    (define-key map (kbd "C-c m s") #'my-music-player-stop)
+    (define-key map (kbd "C-c m n") #'my-music-player-next)
+    (define-key map (kbd "C-c m b") #'my-music-player-previous)
+    (define-key map (kbd "C-c m r") #'my-music-player-repeat-toggle)
+    (define-key map (kbd "C-c m f") #'my-music-player-shuffle-toggle)
+    (define-key map (kbd "C-c m l") #'my-music-player-load-directory)
+    (define-key map (kbd "C-c m i") #'my-music-player-show-current)
+    (define-key map (kbd "C-c m v") #'my-music-player-view-playlist)
+    (define-key map (kbd "C-c m t") #'my-music-player-toggle-streaming)
+    (define-key map (kbd "C-c m q") #'my-music-player-quit)
     map)
   "Keymap for `my-music-player-mode'.")
+;; Simplify which-key display
+(with-eval-after-load 'which-key
+  (push '((nil . "my-music-player-\\(.+\\)") . (nil . "\\1"))
+        which-key-replacement-alist))
 
 (define-minor-mode my-music-player-mode
   "Minor mode for controlling the music player (MP3 and streaming)."
@@ -87,7 +92,7 @@
   (if my-music-player-mode
       (progn
         (my-music-player-initialize)
-        (message "Music Player mode enabled. Use H-m to access commands."))
+        (message "Music Player mode enabled. Use C-c m to access commands."))
     (my-music-player-cleanup)
     (message "Music Player mode disabled.")))
 
@@ -268,19 +273,19 @@
                         (format "Track: %d/%d\n\n" 
                                 (1+ my-mp3-player-index) 
                                 (length my-mp3-player-playlist))))
-            (insert "No playlist loaded. Use H-m l to load a directory.\n")))
+            (insert "No playlist loaded. Use C-c m l to load a directory.\n")))
         (insert "\nControls:\n"
-                "H-m p - Play/Pause\n"
-                "H-m s - Stop\n"
-                "H-m n - Next Track\n"
-                "H-m b - Previous Track\n"
-                "H-m r - Toggle Repeat\n"
-                "H-m f - Toggle Shuffle\n"
-                "H-m l - Load Directory\n"
-                "H-m i - Show Current Track\n"
-                "H-m v - View Playlist\n"
-                "H-m t - Toggle Streaming\n"
-                "H-m q - Quit\n")    
+                "C-c m p - Play/Pause\n"
+                "C-c m s - Stop\n"
+                "C-c m n - Next Track\n"
+                "C-c m b - Previous Track\n"
+                "C-c m r - Toggle Repeat\n"
+                "C-c m f - Toggle Shuffle\n"
+                "C-c m l - Load Directory\n"
+                "C-c m i - Show Current Track\n"
+                "C-c m v - View Playlist\n"
+                "C-c m t - Toggle Streaming\n"
+                "C-c m q - Quit\n")    
         (use-local-map my-music-player-mode-map)
         (setq buffer-read-only t)))))
 
