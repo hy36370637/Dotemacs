d779ca0b5304417c13c6c9403d083d67d43d3f52
HO-YOUNG KIM <hy36370637@gmail.com>
2024-09-22 18:16:48 +0900

    최적화

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 ;;; Config for EMACS
 
 ;; ======================================
--- lisp/my-web-search.el
+++ lisp/my-web-search.el
@@ -4,45 +4,41 @@
 ;; ======================================
 (require 'url)
 (require 'dom)
+
 (defun my/search-selected-text ()
   "Search selected text in macOS Dictionary, Naver, Google, or Namuwiki.
 If the selected text includes '날씨', perform a weather search using my/naver-weather-search."
   (interactive)
-  (let* ((query (if (use-region-p)
+  (let ((query (if (use-region-p)
                     (buffer-substring-no-properties (region-beginning) (region-end))
                   ""))
-         (search-option (completing-read "Choose option: " 
-                                         '("macOS Dictionary" "Naver" "Google" "Namuwiki" "날씨 검색")))
-         (url (cond 
-               ((string-equal search-option "macOS Dictionary")
-                (concat "dict://" (url-hexify-string query)))
-               ((string-equal search-option "Naver")
-                (concat "https://search.naver.com/search.naver?query="
-                        (url-hexify-string query)))
-               ((string-equal search-option "Google")
-                (concat "https://www.google.com/search?q="
-                        (url-hexify-string query)))
-               ((string-equal search-option "Namuwiki")
-                (concat "https://namu.wiki/w/" (url-hexify-string query))))))
+        (search-option (completing-read "Choose option: " 
+                                         '("macOS Dictionary" "Naver" "Google" "Namuwiki" "날씨 검색"))))
     (cond
-     ;; 날씨 검색일 경우, 도시명을 입력받아 검색 수행
      ((string-equal search-option "날씨 검색")
       (let ((city (read-string "도시명을 입력하세요: ")))
         (my/naver-weather-search city)))
-     
-     ;; 다른 옵션을 선택했는데 선택된 텍스트가 없는 경우
      ((and (not (string-equal search-option "날씨 검색")) (string-empty-p query))
       (message "텍스트를 선택해주세요."))
-     
-     ;; 선택된 텍스트가 있는 경우, URL을 열어줍니다.
      (t
-      (if (string-equal search-option "macOS Dictionary")
-          (call-process "open" nil 0 nil url)
-        (browse-url url))))))
+      (let ((url (cond 
+                   ((string-equal search-option "macOS Dictionary")
+                    (concat "dict://" (url-hexify-string query)))
+                   ((string-equal search-option "Naver")
+                    (concat "https://search.naver.com/search.naver?query="
+                            (url-hexify-string query)))
+                   ((string-equal search-option "Google")
+                    (concat "https://www.google.com/search?q="
+                            (url-hexify-string query)))
+                   ((string-equal search-option "Namuwiki")
+                    (concat "https://namu.wiki/w/" (url-hexify-string query))))))
+        (if (string-equal search-option "macOS Dictionary")
+            (call-process "open" nil 0 nil url)
+          (browse-url url)))))))
 
 (defun my/naver-weather-search (city)
   "Search Naver weather information for the given CITY."
-  (let* ((encoded-city (url-hexify-string city)))
+  (let ((encoded-city (url-hexify-string city)))
     (url-retrieve
      (format "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%s%%20날씨" encoded-city)
      (lambda (status city)
@@ -52,13 +48,13 @@ If the selected text includes '날씨', perform a weather search using my/naver-
              (let* ((dom (libxml-parse-html-region (point-min) (point-max)))
                     (temperature-elem (dom-by-class dom "temperature_text"))
                     (temperature (when temperature-elem 
-                                   (replace-regexp-in-string "[ \n]" "" (dom-texts (car temperature-elem)))))
+                                   (string-replace " " "" (dom-texts (car temperature-elem)))))
                     (summary-elem (dom-by-class dom "summary"))
-                    (summary (when summary-elem
-                               (replace-regexp-in-string "[ \n]" "" (dom-texts (car summary-elem)))))
+                    (summary (when summary-elem 
+                               (string-replace " " "" (dom-texts (car summary-elem)))))
                     (weather-elem (dom-by-class dom "weather before_slash"))
-                    (weather (when weather-elem
-                               (replace-regexp-in-string "[ \n]" "" (dom-texts (car weather-elem)))))
+                    (weather (when weather-elem 
+                               (string-replace " " "" (dom-texts (car weather-elem)))))
                     (dust-elems (dom-by-class dom "today_chart_list"))
                     (dust-info (when dust-elems
                                  (cl-remove-duplicates
@@ -76,8 +72,8 @@ If the selected text includes '날씨', perform a weather search using my/naver-
                     (weekly-info (mapcar (lambda (day)
                                            (list (dom-texts (dom-by-class day "date"))
                                                  (dom-texts (dom-by-class day "weather"))
-                                                 (replace-regexp-in-string "최저기온" "" (dom-texts (dom-by-class day "lowest")))
-                                                 (replace-regexp-in-string "최고기온" "" (dom-texts (dom-by-class day "highest")))))
+                                                 (string-replace "최저기온" "" (dom-texts (dom-by-class day "lowest")))
+                                                 (string-replace "최고기온" "" (dom-texts (dom-by-class day "highest")))))
                                          weekly-forecast)))
                (with-current-buffer (get-buffer-create (format "*%s 날씨*" city))
                  (erase-buffer)
@@ -100,9 +96,8 @@ If the selected text includes '날씨', perform a weather search using my/naver-
            (error
             (message "날씨 데이터 파싱 중 오류 발생: %s" err)))))
      (list city)
-     t)))
+     t))))
 
 
-
-;; end here
+;;end here
 (provide 'my-web-search)
