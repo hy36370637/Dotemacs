84e183285ed4e7bd720bcd5a6826bfa2fb17b131
HO-YOUNG KIM <hy36370637@gmail.com>
2025-11-25 03:18:35 +0900

    이동관련 함수 삭제

--- lisp/my-dired-custom.el
+++ lisp/my-dired-custom.el
@@ -5,48 +5,39 @@
 (use-package dired
   :ensure nil
   :custom
-;;  (insert-directory-program "gls")
   (dired-listing-switches "-ah --group-directories-first")
   (dired-dwim-target t) 
   (dired-recursive-copies 'always) 
   (dired-recursive-deletes 'always)
-  (dired-mouse-drag-files t)                  ; Emacs 29.1
+;;  (dired-mouse-drag-files t)                  ; Emacs 29.1
   (dired-free-space nil)               	    ; Emacs 29.1
   (dired-auto-revert-buffer t)
   (delete-by-moving-to-trash t)
   :bind (:map dired-mode-map
-              ("M-<up>" . my/dired-jump-to-top)
-              ("M-<down>" . my/dired-jump-to-bottom)
+              ;; ("M-<up>" . my/dired-jump-to-top)
+              ;; ("M-<down>" . my/dired-jump-to-bottom)
 	      ("C-+" . dired-create-empty-file)
               ("C-<return>" . dired-do-open)
-	      ("C-c C-o" . dired-open-in-finder)
+	      ;; ("C-c C-o" . dired-open-in-finder)
               ("/" . dired-narrow))
 ;;              ("M-p" . dired-mpv-play-file))  ; mpv 재생 단축키 추가
-  :config
-  (defun sof/dired-sort ()
-    "Dired 정렬, 디렉토리를 우선으로."
-    (save-excursion
-      (let (buffer-read-only)
-        (forward-line 2)
-        (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max))))
-    (set-buffer-modified-p nil))
+  ;; :config
+  ;; (defun my/dired-jump-to-top ()
+  ;;   "Dired에서 맨 위로 이동."
+  ;;   (interactive)
+  ;;   (goto-char (point-min))
+  ;;   (dired-next-line 2))
 
-  (defun my/dired-jump-to-top ()
-    "Dired에서 맨 위로 이동."
-    (interactive)
-    (goto-char (point-min))
-    (dired-next-line 2))
+  ;; (defun my/dired-jump-to-bottom ()
+  ;;   "Dired에서 맨 아래로 이동."
+  ;;   (interactive)
+  ;;   (goto-char (point-max))
+  ;;   (dired-next-line -1))
 
-  (defun my/dired-jump-to-bottom ()
-    "Dired에서 맨 아래로 이동."
-    (interactive)
-    (goto-char (point-max))
-    (dired-next-line -1))
-
-  (defun dired-open-in-finder ()
-    "Open current directory in macOS Finder."
-    (interactive)
-    (shell-command (concat "open " (dired-current-directory))))
+  ;; (defun dired-open-in-finder ()
+  ;;   "Open current directory in macOS Finder."
+  ;;   (interactive)
+  ;;   (shell-command (concat "open " (dired-current-directory))))
 
   ;; (defun dired-mpv-play-file ()
   ;;   "Play the file at point with mpv."
