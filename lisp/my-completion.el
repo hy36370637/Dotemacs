76629b07384c5f710fdf696785208f0838697e41
HO-YOUNG KIM <hy36370637@gmail.com>
2025-12-28 08:57:32 +0900

    electric mode 확장

--- lisp/my-completion.el
+++ lisp/my-completion.el
@@ -72,13 +72,107 @@
 ;; ======================================-
 (use-package electric
   :ensure nil
-  :init (electric-pair-mode t)
+  :init
+  (electric-pair-mode t)
   :custom
+  ;; 모든 모드에서 공통으로 사용할 기본 괄호 설정
   (electric-pair-pairs '((?\" . ?\")
                          (?\' . ?\')
                          (?\{ . ?\})
                          (?\[ . ?\])
-                         (?\( . ?\)))))
+                         (?\( . ?\))))
+  :hook
+  ;; Org-mode가 실행될 때만 실행될 로직
+  (org-mode . (lambda ()
+                (setq-local electric-pair-pairs 
+                            (append electric-pair-pairs
+                                    '((?* . ?*)   
+                                      (?/ . ?/)   
+                                      (?= . ?=)   
+                                      (?~ . ?~)   
+                                      (?+ . ?+)))))))
+
+;; =======================================
+;;; 특수문자 입력
+;; ======================================-
+;;inspire https://protesilaos.com
+(defcustom my-pair-pairs
+  '((?* :description "Bold"           :pair ?*)
+    (?/ :description "Italic"         :pair ?/)
+    (?= :description "Verbatim"       :pair ?=)
+    (?~ :description "Code"           :pair ?~)
+    (?+ :description "Strike"         :pair ?+)
+    (?\" :description "Double Quotes" :pair ?\")
+    (?\' :description "Single Quotes" :pair ?\')
+    (?\( :description " () "          :pair (?\( . ?\)))
+    (?\[ :description " [] "          :pair (?\[ . ?\]))
+    (?{  :description " {} "          :pair (?{ . ?}))
+    (?<  :description "「」"          :pair ("「" . "」"))
+    (?>  :description "『』"          :pair ("『" . "』")))
+;;    (?M  :description "《》"          :pair ("《" . "》")))
+  "Org-mode 마커 및 특수 괄호 쌍 리스트"
+  :group 'editing
+  :type '(alist :key-type character :value-type (plist)))
+
+(defun my--enable-tab-escape ()
+  "기호 삽입 후 TAB 키로 한 칸 이동하는 일회성 키맵 활성화."
+  (set-transient-map
+   (let ((map (make-sparse-keymap)))
+     (define-key map (kbd "TAB")
+                 (lambda () (interactive) (forward-char 1)))
+     map)
+   t))
+
+(defun my-region-wrap (char)
+  "선택 영역이나 단어를 입력받은 CHAR 쌍으로 감쌉니다. TAB으로 탈출 가능합니다."
+  (interactive "c기호 입력 (*, /, =, ~, (, [, <, > ...): ")
+  (let* ((entry (assoc char my-pair-pairs))
+         (pair-data (plist-get (cdr entry) :pair))
+         (open (if (consp pair-data) (car pair-data) pair-data))
+         (close (if (consp pair-data) (cdr pair-data) pair-data))
+         (bounds (if (use-region-p)
+                     (cons (region-beginning) (region-end))
+                   (or (bounds-of-thing-at-point 'symbol)
+                       (cons (point) (point)))))
+         (start (car bounds))
+         (end (cdr bounds)))
+
+    (if (not pair-data)
+        (message "정의되지 않은 기호: %c" char)
+      (save-excursion
+        ;; 닫는 기호 삽입
+        (goto-char end)
+        (insert (if (characterp close) (char-to-string close) close))
+        ;; 여는 기호 삽입
+        (goto-char start)
+        (insert (if (characterp open) (char-to-string open) open)))
+      
+      ;; 커서 위치 조정 및 TAB 탈출 기능 활성화
+      (unless (use-region-p) (goto-char (1+ end)))
+      (my--enable-tab-escape)
+      (message "'%s' 완료 (TAB으로 탈출)" (plist-get (cdr entry) :description)))))
+
+;; =======================================
+;;; Hunspell 설정
+;; ======================================-
+(defun my-korean-spell-check ()
+  "Flyspell 모드를 한국어 사전과 함께 활성화합니다."
+  (interactive)
+  (require 'ispell) ;; 함수 실행 시 패키지 로드
+  (setq ispell-local-dictionary "ko_KR")
+  (flyspell-mode 1)
+  (message "Korean spell check enable"))
+
+(use-package ispell
+  :if my-macOS-p
+  :defer t
+  :config
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary-alist
+        '(("ko_KR" "[가-힣]" "[^가-힣]" "[-']" nil ("-d" "ko_KR") nil utf-8)))
+  (setq flyspell-delay 0.5)
+  (setq flyspell-issue-message-flag nil)
+  (setq flyspell-use-meta-tab nil))
 
 ;; =======================================
 ;;; completion-preview
